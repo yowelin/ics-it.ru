2ba53b718929f6d9dd13fe79a365e75ecb680611
yowelin <semikin.in@inbox.ru>
2025-07-10 20:59:46 +0300

    Update ImportFileCustomerSeasonal.sql

--- ImportFileCustomerSeasonal.sql
+++ ImportFileCustomerSeasonal.sql
@@ -1,26 +1,27 @@
 create procedure syn.usp_ImportFileCustomerSeasonal
 	@Record_ID int
-AS
+as
 set nocount on
 begin
-	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
+	declare @RowCount int = (
+		select count(*)
+		from syn.SA_CustomerSeasonal
+	)
 	declare @ErrorMessage varchar(max)
 
--- Проверка на корректность загрузки
+	-- Проверка на корректность загрузки
 	if not exists (
-	select 1
-	from syn.ImportFile as f
-	where f.ID = @Record_ID
-		and f.FlagLoaded = cast(1 as bit)
+		select 1
+		from syn.ImportFile as f
+		where f.ID = @Record_ID
+			and f.FlagLoaded = cast(1 as bit)
 	)
-		begin
-			set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
+		set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
 
-			raiserror(@ErrorMessage, 3, 1)
-			return
-		end
+		raiserror(@ErrorMessage, 3, 1)
+		return
 
-	--Чтение из слоя временных данных
+	-- Чтение из слоя временных данных
 	select
 		c.ID as ID_dbo_Customer
 		,cst.ID as ID_CustomerSystemType
@@ -30,12 +31,12 @@ begin
 		,c_dist.ID as ID_dbo_CustomerDistributor
 		,cast(isnull(cs.FlagActive, 0) as bit) as FlagActive
 	into #CustomerSeasonal
-	from syn.SA_CustomerSeasonal cs
-		join dbo.customer as c on c.UID_DS = cs.UID_DS_Customer
-			and c.ID_mapping_DataSource = 1
+	from syn.SA_CustomerSeasonal as cs
+		join dbo.customer as c on cs.UID_DS_Customer = c.UID_DS
+			and c_dist.ID_mapping_DataSource = 1
 		join dbo.Season as s on s.Name = cs.Season
-		join dbo.Customer as c_dist on c_dist.UID_DS = cs.UID_DS_CustomerDistributor
-			and cd.ID_mapping_DataSource = 1
+		join dbo.Customer as c_dist on cs.UID_DS_CustomerDistributor = c_dist.UID_DS
+			and c_dist.ID_mapping_DataSource = 1
 		join syn.CustomerSystemType as cst on cs.CustomerSystemType = cst.Name
 	where try_cast(cs.DateBegin as date) is not null
 		and try_cast(cs.DateEnd as date) is not null
@@ -56,13 +57,13 @@ begin
 		end as Reason
 	into #BadInsertedRows
 	from syn.SA_CustomerSeasonal as cs
-	left join dbo.Customer as c on c.UID_DS = cs.UID_DS_Customer
+	left join dbo.customer as c on cs.UID_DS_Customer = c.UID_DS
 		and c.ID_mapping_DataSource = 1
-	left join dbo.Customer as c_dist on c_dist.UID_DS = cs.UID_DS_CustomerDistributor and c_dist.ID_mapping_DataSource = 1
+	left join dbo.Customer as c_dist on cs.UID_DS_CustomerDistributor = c_dist.UID_DS and c_dist.ID_mapping_DataSource = 1
 	left join dbo.Season as s on s.Name = cs.Season
 	left join syn.CustomerSystemType as cst on cst.Name = cs.CustomerSystemType
-	where cc.ID is null
-		or cd.ID is null
+	where c.ID is null
+		or c_dist.ID is null
 		or s.ID is null
 		or cst.ID is null
 		or try_cast(cs.DateBegin as date) is null
@@ -70,7 +71,7 @@ begin
 		or try_cast(isnull(cs.FlagActive, 0) as bit) is null
 
 	-- Обработка данных из файла
-	merge into syn.CustomerSeasonal as cs
+	merge syn.CustomerSeasonal as t
 	using (
 		select
 			cs_temp.ID_dbo_Customer
@@ -84,8 +85,7 @@ begin
 	) as s on s.ID_dbo_Customer = cs.ID_dbo_Customer
 		and s.ID_Season = cs.ID_Season
 		and s.DateBegin = cs.DateBegin
-	when matched
-		and t.ID_CustomerSystemType <> s.ID_CustomerSystemType then
+	when matched and t.ID_CustomerSystemType <> s.ID_CustomerSystemType then
 		update
 		set
 			ID_CustomerSystemType = s.ID_CustomerSystemType
@@ -94,7 +94,7 @@ begin
 			,FlagActive = s.FlagActive
 	when not matched then
 		insert (ID_dbo_Customer, ID_CustomerSystemType, ID_Season, DateBegin, DateEnd, ID_dbo_CustomerDistributor, FlagActive)
-		values (s.ID_dbo_Customer, s.ID_CustomerSystemType, s.ID_Season, s.DateBegin, s.DateEnd, s.ID_dbo_CustomerDistributor, s.FlagActive)
+		values (s.ID_dbo_Customer, s.ID_CustomerSystemType, s.ID_Season, s.DateBegin, s.DateEnd, s.ID_dbo_CustomerDistributor, s.FlagActive);
 
 	-- Информационное сообщение
 	begin
